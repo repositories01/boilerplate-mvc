02ca2ca1b54e4b82d74e30eb2092e2a904f43fef
thiagomedina <thiagomedina001@gmail.com>
2020-06-24 17:55:21 -0300

    comentarios

--- app/controller/HomeController.php
+++ app/controller/HomeController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -13,27 +12,61 @@ class HomeController
   public function index(Request $request, Response $response)
   {
 
+    // $this->dados = HomeModel::all();
+    
+   $home= include '../app/views/home.php';
+   $response->getBody()->getContents($home);
   }
-
-
+  
+  
   public function salvar(Request $request, Response $response)
   {
-
-  }
-
-  public function editar(Request $request, Response $response)
-  {
-
-  }
-
-  public function atualizar(Request $request, Response $response)
-  {
-
-  }
-
-
-  public function deletar(Request $request, Response $response)
-  {
+    // $dados = $request->getParsedBody();
     
-  }
+    // $task = new HomeModel;
+
+    // $task->tarefas = $dados['tarefas'];
+    // $objtask = $task->save();
+
+    // return $response->withRedirect('/'); 
+    
+   $home= include '../app/views/home.php';
+   $response->getBody()->getContents($home );
+}
+
+public function editar( Request $request, Response $response)
+{
+  // $id = $request->getAttribute('id');
+
+  // $editar = HomeModel::find($id);
+
+
+  // $this->editar = $editar;
+
+
+  $editar= include '../app/views/editar.php';
+  $response->getBody()->getContents($editar );
+}
+
+public function atualizar(Request $request, Response $response)
+{
+  // $dados = $request->getParsedBody();
+  // $id = $request->getAttribute('id');
+  // $atualizar = HomeModel::find($id);
+  // $atualizar->tarefas = $dados['tarefas'];
+  // $objAtualizar = $atualizar->save();
+
+  return $response->withRedirect('/');
+}
+
+
+public function deletar(Request $request, Response $response)
+{
+  // $id = $request->getAttribute('id');
+  // $deletar = HomeModel::find($id);
+  // $deletarOk = $deletar->delete();
+
+  return $response->withRedirect('/');
+}
+
 }
--- app/views/Home.php
+++ app/views/Home.php
@@ -2,13 +2,32 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>HOME</title>
+    <title>Principal</title>
   </head>
   <body>
-    <h2>MVC</h2>
-  
-<?php
+    <h2>Lista de tarefas</h2>
    
+
+<form action="/home/cadastro" method="post">
+<input type="text" name="tarefas" />
+<button>adicionar</button>
+
+</form>
+<?php
+    $user = $this->dados;
+
+  
+foreach($user as $key => $value)  {
+  echo "<li>".$value['tarefas'] . "</li>";
+  ?>
+    <a href="/home/editar/<?php echo  $value['id']?>"><button type="button">editar</button></a>
+
+    <form action="/home/deletar/<?php echo $value['id']?>" method="post"> 
+     <input type="hidden"  name="_METHOD" value="delete">
+  <button>deletar</button>
+  </form>
+  <?php
+}
 
   ?>
 
